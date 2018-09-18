19ef3d279f58b2e5edc0cbb0fcc6bf907c2f8590
Jennifer Petzoldt <j.petzoldt@gmail.com>
2018-09-18 11:07:41 -0500

    adds handle change functions to SecondHourClassNameInput

--- src/components/SecondHourClassNameInput/SecondHourClassNameInput.js
+++ src/components/SecondHourClassNameInput/SecondHourClassNameInput.js
@@ -5,16 +5,46 @@ const mapStateToProps = state => ({
 });
 
 class SecondHourClassNameInput extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            levelTwo: '',
+            levelThree: '',
+            levelFive: '',
+        }
+    }
+
+    handleLevelTwoChange = (event) => {
+        this.setState({
+            levelTwo: event.target.value,
+        });
+    }
+
+    handleLevelThreeChange = (event) => {
+        this.setState({
+            levelThree: event.target.value,
+        })
+    }
+
+    handleLevelFiveChange = (event) => {
+        this.setState({
+            levelFive: event.target.value,
+        })
+        console.log('state equals', this.state);
+    }
+
+    handleLevel
+
     render() {
         return (
             <div>
                 <h3>08:30pm</h3>
                 <label>Level 2</label>
-                <input />
+                <input onChange={this.handleLevelTwoChange}/>
                 <label>Level 3</label>
-                <input />
+                <input onChange={this.handleLevelThreeChange}/>
                 <label>Level 5</label>
-                <input />
+                <input onChange={this.handleLevelFiveChange}/>
             </div>
         )
     }
