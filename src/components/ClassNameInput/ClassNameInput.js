1f2c0f3d54bbe4052b01c6d148097c60635878da
Jennifer Petzoldt <j.petzoldt@gmail.com>
2018-09-18 11:49:50 -0500

    combines form into one component and client side POST route

---
+++ src/components/ClassNameInput/ClassNameInput.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
+
+const mapStateToProps = state => ({
+});
+
+class ClassNameInput extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          newForm: { 
+            month: '',
+            year: '',
+            levelOne: '',
+            levelFour: '',
+            soloJazz: '',
+            levelTwo: '',
+            levelThree: '',
+            levelFive: '',}
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            newForm: {
+                ...this.state.newForm,
+                [event.target.name]: event.target.value,
+            }
+        });
+    }
+
+    addNewForm = event => {
+        event.preventDefault();
+        this.props.dispatch({type: 'ADD_FORM', payload: this.state.newForm});
+        axios({
+            method: 'POST',
+            url: '/api/form',
+            data: {newForm: this.state.newForm}
+        }).then((response) =>{
+            console.log('Success with form POST');
+        }).catch((error)=>{
+            console.log('Post error', error);
+            alert('Unable to add form');
+        });
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.addNewForm}>
+                <div>
+                    <label>Month</label>
+                    <input name="month" onChange={this.handleChange} />
+                    <label>Year</label>
+                    <input name="year" onChange={this.handleChange} />
+                    Tuesdays
+                </div>
+                <h2>Lessons</h2>
+                <div>
+                    <h3>07:00pm</h3>
+                    <label>Level 1</label>
+                    <input name="levelOne" onChange={this.handleChange} />
+                    <label>Level 4</label>
+                    <input name="levelFour" onChange={this.handleChange} />
+                    <label>Solo Jazz</label>
+                    <input name="soloJazz" onChange={this.handleChange} />
+                    <h3>08:30pm</h3>
+                    <label>Level 2</label>
+                    <input name="levelTwo" onChange={this.handleChange}  />
+                    <label>Level 3</label>
+                    <input name="levelThree" onChange={this.handleChange}  />
+                    <label>Level 5</label>
+                    <input name="levelFive" onChange={this.handleChange}  />
+                    <input type="submit" value="Submit" />
+                </div>
+            </form>
+        )
+    }
+}
+
+export default connect(mapStateToProps)(ClassNameInput);
--- src/components/CreateFormPage/CreateFormPage.js
+++ src/components/CreateFormPage/CreateFormPage.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Nav from '../Nav/Nav';
-import MonthHeaderInput from '../MonthHeaderInput/MonthHeaderInput';
-import FirstHourClassNameInput from '../FirstHourClassNameInput/FirstHourClassNameInput';
-import SecondHourClassNameInput from '../SecondHourClassNameInput/SecondHourClassNameInput';
+
+
 
 const mapStateToProps = state => ({
 });
@@ -13,10 +12,32 @@ class CreateFormPage extends Component {
         return (
             <div>
                 <Nav />
-                <MonthHeaderInput />
+                <div>
+                    <label>Month</label>
+                    <input />
+                    <label>Year</label>
+                    <input />
+                    Tuesdays
+                </div>
                 <h2>Lessons</h2>
-                <FirstHourClassNameInput />
-                <SecondHourClassNameInput />
+                <div>
+                    <h3>07:00pm</h3>
+                    <label>Level 1</label>
+                    <input onChange={this.handleLevelOneChange} />
+                    <label>Level 4</label>
+                    <input onChange={this.handleLevelFourChange} />
+                    <label>Solo Jazz</label>
+                    <input onChange={this.handleSoloJazzChange} />
+                    <h3>08:30pm</h3>
+                    <label>Level 2</label>
+                    <input onChange={this.handleLevelTwoChange} />
+                    <label>Level 3</label>
+                    <input onChange={this.handleLevelThreeChange} />
+                    <label>Level 5</label>
+                    <input onChange={this.handleLevelFiveChange} />
+                    <button>Create Form</button>
+                </div>
+
                 <button>Create Form</button>
             </div>
         )
