1c4d4c46fc64dbbd338a5e6b15c58811f68fcf46
Jennifer Petzoldt <j.petzoldt@gmail.com>
2018-09-21 06:57:41 -0500

    adds user auth to CreateFormPage

--- src/components/CreateFormPage/CreateFormPage.js
+++ src/components/CreateFormPage/CreateFormPage.js
@@ -3,19 +3,29 @@ import { connect } from 'react-redux';
 import Nav from '../Nav/Nav';
 import ClassNameInput from '../ClassNameInput/ClassNameInput';
 
-
-
 const mapStateToProps = state => ({
+    user: state.user,
+    state,
 });
 
 class CreateFormPage extends Component {
     render() {
+        let content = null;
+
+        if (this.props.user.userName) {
+            content = (
+                <div>
+                    <ClassNameInput />
+                </div>
+            );
+        }
+
         return (
             <div>
                 <Nav />
-                <ClassNameInput />
+                {content}
             </div>
-        )
+        );
     }
 }
 
