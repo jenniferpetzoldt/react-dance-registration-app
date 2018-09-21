a348b726fb91d5ff5fab6d5fa5a0d230a869caa0
Jennifer Petzoldt <j.petzoldt@gmail.com>
2018-09-21 06:47:58 -0500

    adds user auth to attendancePage render

--- src/components/AddDancer/AddDancer.js
+++ src/components/AddDancer/AddDancer.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Nav from '../Nav/Nav';
+
 
 const mapStateToProps = state => ({
     user: state.user,
--- src/components/AttendancePage/AttendancePage.js
+++ src/components/AttendancePage/AttendancePage.js
@@ -6,22 +6,33 @@ import AttendanceTable from '../AttendanceTable/AttendanceTable';
 // import {Select, Button, TextField, Table,  } from '@material-ui/core';
 
 const mapStateToProps = state => ({
+    user: state.user,
+    state,
 });
 
 class AttendancePage extends Component {
     render() {
+        let content = null;
+
+        if (this.props.user.userName) {
+            content = (
+                <div>
+                    <div>
+                        <select></select>
+                    </div>
+                    <div>
+                        <AddDancer />
+                    </div>
+                    <div>
+                        <AttendanceTable />
+                    </div>
+                </div>
+            );
+        }
         return (
             <div>
                 <Nav />
-                <div>
-                    <select></select>
-                </div>
-                <div>
-                    <AddDancer />
-                </div>
-                <div>
-                    <AttendanceTable />
-                </div>
+                {content}
             </div>
 
         )
