2c58504c2223e8fca65908116a203961793c3edc
Jennifer Petzoldt <j.petzoldt@gmail.com>
2018-09-23 14:06:20 -0500

    sorts created forms ascending by date

--- server/routes/created.router.js
+++ server/routes/created.router.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     if(req.isAuthenticated()) {
-        const query = `SELECT * FROM "wed_form";`;
+        const query = `SELECT * FROM "wed_form" ORDER BY "start_date" ASC;`;
         pool.query(query)
         .then((response) => {
             console.log('created forms GET:', response.rows);
