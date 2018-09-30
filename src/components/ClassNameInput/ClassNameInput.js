c9283b936ac0f2c3cda3652d4a13c57a45947198
Jennifer Petzoldt <j.petzoldt@gmail.com>
2018-09-29 22:47:14 -0500

    adds new favcon and adds speces to class name inputs

--- src/components/ClassNameInput/ClassNameInput.js
+++ src/components/ClassNameInput/ClassNameInput.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
 import Nav from '../Nav/Nav';
-import { TextField, FormControl, Button } from '@material-ui/core';
+import { TextField, FormControl, Grid, Button } from '@material-ui/core';
 
 const mapStateToProps = state => ({
     user: state.user,
     state,
 });
+
+const styles = {
+    formControl: {
+        margin: 15,
+    },
+    textField: {
+        width: 55,
+    }
+}
 
 class ClassNameInput extends Component {
     constructor(props) {
@@ -62,36 +71,36 @@ class ClassNameInput extends Component {
                     <form>
                         <h2>Class Names:</h2>
                         <p>Input class names including level below:</p>
-                        <div>
-                            <FormControl>
+                        <Grid>
+                            <FormControl style={styles.formControl}>
                                 <TextField label="Level 1" name="levelOne" value={this.state.newForm.levelOne} 
                                             onChange={this.handleChange} />
                             </FormControl>
-                            <FormControl>
+                            <FormControl style={styles.formControl}>
                                 <TextField label="Level 2" name="levelTwo" value={this.state.newForm.levelTwo}
                                             onChange={this.handleChange} />
                             </FormControl>
-                            <FormControl>
+                            <FormControl style={styles.formControl}>
                                 <TextField label="Level 3"  name="levelThree" value={this.state.newForm.levelThree}
                                             onChange={this.handleChange} />
                             </FormControl>
                             <br />
-                            <FormControl>
+                            <FormControl style={styles.formControl}>
                                 <TextField label="Level 4" name="levelFour"value={this.state.newForm.levelFour}
                                             onChange={this.handleChange} />
                             </FormControl>
-                            <FormControl>
+                            <FormControl style={styles.formControl}>
                                 <TextField label="Level 5" name="levelFive" value={this.state.newForm.levelFive}
                                             onChange={this.handleChange} />
                             </FormControl>
-                            <FormControl>
+                            <FormControl style={styles.formControl}>
                                 <TextField label="Solo Jazz" name="soloJazz" value={this.state.newForm.soloJazz}
                                             onChange={this.handleChange} />
                             </FormControl>
                             <br />
                             <Button className="submit" varient="raised" onClick={this.addNewForm}>Submit</Button>
                             <Button className="cancel" varient="raised">Cancel</Button>
-                        </div>
+                        </Grid>
                     </form>
                 </div >
             );
